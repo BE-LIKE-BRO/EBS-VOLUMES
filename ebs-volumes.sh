d0388c3c9a2fa7ed00ba31eddb55f95b767bca64
Ayoola Coulibaly <ayoolacoul27@gmail.com>
2022-03-06 21:40:32 +0100

    updated command to list unencrypted volumes

--- ebs-volumes.sh
+++ ebs-volumes.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 #connect to aws environment
 aws configure
+#Compiling all unencrypted volumes in a file
+echo "compiling unencrypted volumes' IDs in file " 
+aws ec2 describe-volumes --output text --filters "Name=encrypted, Values=false" --query 'Volumes[*].Attachments[].{VolumeID:VolumeId,InstanceID:InstanceId}' > volumes.txt

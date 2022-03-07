92418f652c7ff5349a92834f68707f138e5c8f90
Ayoola Coulibaly <ayoolacoul27@gmail.com>
2022-03-07 05:31:15 +0100

    fixed list_encrypted_volumes.sh

--- list_encrypted_volumes.sh
+++ list_encrypted_volumes.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Compiling all encrypted volumes in a file
 echo "compiling encrypted volumes' IDs in file... " 
-$ aws ec2 describe-volumes --output text --filters "Name=encrypted, Values=true" --query 'Volumes[*].{VolumeID:VolumeId, SnapshotId:SnapshotId}' > encrypted_volumes.txt
+aws ec2 describe-volumes --output text --filters "Name=encrypted, Values=true" --query 'Volumes[*].{VolumeID:VolumeId, SnapshotId:SnapshotId}' > encrypted_volumes.txt
 echo "List of all encrypted volumes compiled in file encrypted_volumes.txt'. " 
 
--- main.sh
+++
@@ -1,2 +0,0 @@
-#!bin/bash
-source ./create_snapshots.sh
